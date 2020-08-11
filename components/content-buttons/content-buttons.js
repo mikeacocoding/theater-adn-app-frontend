5d3cde500f7daaf5dd3afa832b6533337239063c
Miguel Acosta <dev.miguel.acosta@gmail.com>
2020-08-10 19:54:10 -0500

    Cambio de dependencias, a Material

--- components/content-buttons/content-buttons.js
+++ components/content-buttons/content-buttons.js
@@ -1,10 +1,20 @@
 import styles from './content-buttons.module.css';
-import { Button } from 'react-bootstrap';
 import Link from 'next/link';
+import Button from '@material-ui/core/Button';
 
-const ContentButtons = () => <div className={styles.container}>
-  <Link href="/movies" ><Button variant="outline-primary" size="lg">Movies</Button></Link>
-  <Link href="/movietickets" ><Button variant="outline-primary" size="lg">Movie Tickets</Button></Link>
-</div>;
+const ContentButtons = () => (
+  <div className={styles.container}>
+    <Link href='/movies'>
+      <Button size="large" variant='contained' color='primary'>
+        Movies
+      </Button>
+    </Link>
+    <Link href='/movietickets'>
+      <Button size="large" variant='outlined' color='primary'>
+        Movie Tickets
+      </Button>
+    </Link>
+  </div>
+);
 
 export default ContentButtons;
